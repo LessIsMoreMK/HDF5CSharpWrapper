742776274b9c0b0ee0147e248777cd5111937065
Maciej Kulaszewicz <maciej8kz@gmail.com>
2022-02-11 10:48:50 +0100

    Remove error messages from HDF.Pinvoke package

--- HDF5CSharpWrapper/File.cs
+++ HDF5CSharpWrapper/File.cs
@@ -1,4 +1,5 @@
 ﻿using HDF.PInvoke;
+using System;
 
 namespace HDF5CSharpWrapper
 {
@@ -14,7 +15,13 @@ namespace HDF5CSharpWrapper
         /// <param name="fileMode">A mode in which file is opened ReadWrite/ReadOnly</param>
         /// <returns>File ID</returns>
         public long OpenFile(string filename, FileMode fileMode = FileMode.ReadWrite)
-            => H5F.open(filename, (uint)fileMode);
+        {
+            var stackid = H5E.get_current_stack();
+            H5E.auto_t auto_cb = ErrorDelegateMethod;
+            H5E.set_auto(H5E.DEFAULT, auto_cb, IntPtr.Zero);
+
+            return H5F.open(filename, (uint)fileMode);
+        }
 
         /// <summary>
         /// Creates a HDF5 file
@@ -23,7 +30,13 @@ namespace HDF5CSharpWrapper
         /// <param name="fileMode">A mode in which file is created WriteNew/WriteIfNew</param>
         /// <returns>File ID</returns>
         public long CreateFile(string filename, FileMode fileMode = FileMode.WriteNew)
-            => H5F.create(filename, (uint)fileMode);
+        {
+            var stackid = H5E.get_current_stack();
+            H5E.auto_t auto_cb = ErrorDelegateMethod;
+            H5E.set_auto(H5E.DEFAULT, auto_cb, IntPtr.Zero);
+
+            return H5F.create(filename, (uint)fileMode);
+        }
 
         /// <summary>
         /// Close a HDF5 file
@@ -32,5 +45,17 @@ namespace HDF5CSharpWrapper
         /// <returns>0 if successful, -1 if not</returns>
         public long CloseFile(long fileId)
             => H5F.close(fileId);
+
+
+        private int CustomErrorMessage(uint n, ref H5E.error_t err_desc, IntPtr client_data)
+        {
+            //Console.WriteLine("Custom error list*");
+            return 0;
+        }
+        private int ErrorDelegateMethod(long estack, IntPtr client_data)
+        {
+            H5E.walk(H5E.DEFAULT, H5E.direction_t.H5E_WALK_UPWARD, CustomErrorMessage, IntPtr.Zero);
+            return -1;
+        }
     }
 }
